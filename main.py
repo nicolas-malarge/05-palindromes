70479e11e1af6fbdf0ee890e77060cde896d026e
nicolas-malarge <nicolas.malarge@edu.esiee.fr>
2024-11-14 11:00:50 +0000

    palindrome

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+"""
+Trouve si une chaine de caractère est un palindrome
+"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """
+    Retourne True si la phrase est un palindrome
+    """
+    p = p.lower()
+    phrase = p.maketrans("êéëèâàôûç","eeeeaaouc","-?' /!:[];,}{")
+    p = p.translate(phrase)
+    if p == p[::-1]:
+        return True
     return False
+
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    main
+    """
     # vos appels à la fonction secondaire ici
-
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
